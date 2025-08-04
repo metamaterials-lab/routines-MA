15c5ca444b8fb5763fc6bd9941700a672df467f7
LuisDFJ <luis.fuju@outlook.com>
2025-08-04 12:58:49 -0600

    First Commit

---
+++ nylon/__init__.py
@@ -0,0 +1,20 @@
+from utils.Processor.ExperimentTypes import ExperimentTypes
+
+
+def process():
+    from utils.Processor.Instron import Instron
+    from utils.Processor.Shimadzu import Shimadzu
+    from utils.Processor import parse_data
+
+    ### COMPRESSION LDFJ ###
+
+    ### COMPRESSION VC ###
+
+    ### BENDING LDFJ ###
+
+
+def plot():
+    pass
+
+def analyze():
+    pass
